89d3058df56420df530eef82c040c3c988e213a2
My website <nitish.edu.in@gmail.com>
2023-08-27 11:46:54 +0530

    2d

---
+++ Array2D/create2d.java
@@ -0,0 +1,14 @@
+// package Array2D;
+
+public class create2d {
+    public static void main(String[] args) {
+        // 1st method of creating 2d arrays
+
+        int[][] arr = {{2,4,5}, {8,7,9}, {56,45,74}};
+        System.out.println(arr.length);
+
+        // 2nd method to create 
+        int[][] arr2d = new int[5][];
+
+    }
+}
