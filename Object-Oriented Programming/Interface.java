34cb605fcc47481b5e3cacd505717f4a05ed7794
My website <nitish.edu.in@gmail.com>
2023-09-10 20:56:49 +0530

    oops

--- Object-Oriented Programming/Interface.java
+++ Object-Oriented Programming/Interface.java
@@ -1,13 +1,29 @@
 public class Interface {
     public static void main(String[] args) {
-        
+        // classA1 ob = new classA1();
+        classB ob = new classB();
+        ob.method1();
+        ob.method2();
     }
     
 }
 
-/**
- * InnerInterface
- */
-public interface InnerInterface {
-    
+interface classA1 {
+    void method1();
+
+}
+
+interface classA2 {
+    void method2();
+
+}
+
+class classB implements classA1, classA2{
+    public void method1(){
+        System.out.println("hello");
+    }
+
+    public void method2(){
+        System.out.println("print anything");
+    }
 }
