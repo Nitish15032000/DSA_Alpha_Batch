2a827d2fb4ac7905768d8b1ed8c8fd09d2d47b6e
My website <nitish.edu.in@gmail.com>
2023-09-22 22:22:19 +0530

    Backtracking Array

---
+++ Backtracking/ChangeArray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ChangeArray {
+    static void changeArr(int[] arr, int i, int value){
+        // base condition
+        if (i == arr.length) {
+            System.out.println(Arrays.toString(arr));
+            return;
+        }
+        
+        // recursion
+        arr[i] = value;
+        changeArr(arr, i+1, value+1);
+        arr[i] = arr[i]-2;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        
+        changeArr(arr, 0, 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
